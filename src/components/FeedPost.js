112439b516981aeec20883e53443715d103fa732
Nevil844 <nevil.j@ahduni.edu.in>
2024-07-09 18:00:42 +0530

    frontend

---
+++ src/components/FeedPost.js
@@ -0,0 +1,162 @@
+import { Image, StyleSheet, Text, View, Pressable } from "react-native";
+import {
+  Entypo,
+  AntDesign,
+  FontAwesome5,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
+import { useState } from "react";
+import LikeImage from "../../assets/images/like.png";
+import { useNavigation } from "@react-navigation/native";
+
+const FeedPost = ({ post }) => {
+  const navigation = useNavigation();
+
+  const [isLiked, setIsLiked] = useState(false);
+
+  return (
+    <Pressable style={styles.post}>
+      <Pressable
+        onPress={() => navigation.navigate("Profile", { id: post.User.id })}
+        style={styles.header}
+      >
+        <Image source={{ uri: post.User.image }} style={styles.profileImage} />
+        <View>
+          <Text style={styles.name}>{post.User.name}</Text>
+          <Text style={styles.subtitle}>{post.createdAt}</Text>
+        </View>
+        <Entypo
+          name="dots-three-horizontal"
+          size={18}
+          color="gray"
+          style={styles.icon}
+        />
+      </Pressable>
+
+      {post.description && (
+        <Text style={styles.description}>{post.description}</Text>
+      )}
+      {post.image && (
+        <Image source={{ uri: post.image }} style={styles.image} />
+      )}
+
+      <View style={styles.footer}>
+        <View style={styles.statsRow}>
+          <Image source={LikeImage} style={styles.likeIcon} />
+          <Text style={styles.likedBy}>
+            Nevil Jobanputra and {post.numberOfLikes} others
+          </Text>
+          <Text style={styles.shares}>{post.numberOfShares} shares</Text>
+        </View>
+        <View style={styles.buttonsRow}>
+          <Pressable
+            onPress={() => setIsLiked(!isLiked)} //<- onPress event toggles isLiked
+            style={styles.iconButton}
+          >
+            <AntDesign
+              name="like2"
+              size={18}
+              color={isLiked ? "royalblue" : "gray"}
+            />
+            <Text
+              style={[
+                styles.iconButtonText,
+                { color: isLiked ? "royalblue" : "gray" },
+              ]}
+            >
+              Like
+            </Text>
+          </Pressable>
+          <View style={styles.iconButton}>
+            <FontAwesome5 name="comment-alt" size={18} color="gray" />
+            <Text style={styles.iconButtonText}>Comment</Text>
+          </View>
+          <View style={styles.iconButton}>
+            <MaterialCommunityIcons
+              name="share-outline"
+              size={18}
+              color="gray"
+            />
+            <Text style={styles.iconButtonText}>Share</Text>
+          </View>
+        </View>
+      </View>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  post: {
+    width: "100%",
+    marginVertical: 10,
+    backgroundColor: "#fff",
+  },
+  header: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+  },
+  profileImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    marginRight: 10,
+  },
+  name: {
+    fontWeight: "500",
+  },
+  subtitle: {
+    color: "gray",
+  },
+  icon: {
+    marginLeft: "auto",
+  },
+  description: {
+    paddingHorizontal: 10,
+    lineHeight: 20,
+    letterSpacing: 0.3,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+    marginTop: 10,
+  },
+  footer: {
+    paddingHorizontal: 10,
+  },
+  statsRow: {
+    paddingVertical: 10,
+    flexDirection: "row",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor: "lightgray",
+  },
+  likeIcon: {
+    width: 20,
+    height: 20,
+    marginRight: 5,
+  },
+  likedBy: {
+    color: "gray",
+  },
+  shares: {
+    marginLeft: "auto",
+    color: "gray",
+  },
+  buttonsRow: {
+    marginVertical: 10,
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  iconButton: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  iconButtonText: {
+    marginLeft: 5,
+    color: "gray",
+    fontWeight: "500",
+  },
+});
+
+export default FeedPost;
